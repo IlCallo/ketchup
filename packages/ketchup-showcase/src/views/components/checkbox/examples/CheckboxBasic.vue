40e606bebf91dcebd8474f861d9643bf3e2c00a1
foscili-smeup <lucafoscili@icloud.com>
2019-12-09 11:00:52 +0100

    Added custom class to checkbox examples ins showcase.

--- packages/ketchup-showcase/src/views/components/checkbox/examples/CheckboxBasic.vue
+++ packages/ketchup-showcase/src/views/components/checkbox/examples/CheckboxBasic.vue
@@ -4,9 +4,7 @@
 
     <h3>Base</h3>
     <div class="example-container">
-      <kup-checkbox
-        :checked.prop="base"
-        @kupCheckboxChange="changeBase"/>
+      <kup-checkbox :checked.prop="base" @kupCheckboxChange="changeBase" />
       <code>Checked: {{ base }}</code>
     </div>
 
@@ -16,8 +14,12 @@
         :checked.prop="cbChecked.checked"
         @kupCheckboxBlur="cbChecked.focused = false"
         @kupCheckboxChange="changeChecked"
-        @kupCheckboxFocus="cbChecked.focused = true"/>
-      <code>Checked: {{ cbChecked.checked }}<br>Focused: {{ cbChecked.focused }}</code>
+        @kupCheckboxFocus="cbChecked.focused = true"
+      />
+      <code
+        >Checked: {{ cbChecked.checked }}<br />Focused:
+        {{ cbChecked.focused }}</code
+      >
     </div>
 
     <h3>Disabled</h3>
@@ -25,40 +27,48 @@
       <kup-checkbox
         :checked.prop="cbDisabled"
         disabled
-        @kupCheckboxChange="changeDisabled"/>
+        @kupCheckboxChange="changeDisabled"
+      />
       <code>Checked: {{ cbDisabled }}</code>
     </div>
 
+    <h3>Custom</h3>
+    <div class="example-container">
+      <kup-checkbox
+        custom
+        :checked.prop="base"
+        @kupCheckboxChange="changeBase"
+      />
+      <code>Checked: {{ cbDisabled }}</code>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "KupCheckboxExamples",
-    data() {
-      return {
-        base: false,
-        cbChecked: {
-          checked: true,
-          focused: false,
-        },
-        cbDisabled: true,
-      }
+export default {
+  name: 'KupCheckboxExamples',
+  data() {
+    return {
+      base: false,
+      cbChecked: {
+        checked: true,
+        focused: false,
+      },
+      cbDisabled: true,
+    };
+  },
+  methods: {
+    changeBase(e) {
+      this.base = e.detail.checked;
     },
-    methods: {
-      changeBase(e) {
-        this.base = e.detail.checked;
-      },
-      changeChecked(e) {
-        this.cbChecked.checked = e.detail.checked;
-      },
-      changeDisabled(e) {
-        this.cbDisabled = e.detail.checked;
-      }
-    }
-  }
+    changeChecked(e) {
+      this.cbChecked.checked = e.detail.checked;
+    },
+    changeDisabled(e) {
+      this.cbDisabled = e.detail.checked;
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
